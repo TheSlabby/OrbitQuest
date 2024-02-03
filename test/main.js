8dae15c9999020e9c02396ffba1efec04b8f751a
Walker <walker.mcgilvary@gmail.com>
2024-02-03 14:09:05 -0600

    object

--- test/main.js
+++ test/main.js
@@ -1,7 +1,9 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.133.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.133.0/examples/jsm/loaders/GLTFLoader.js';
+
 
 let camera, scene, renderer;
-let mesh;
+let earth, iss;
 
 init();
 animate();
@@ -14,14 +16,59 @@ function init() {
     scene = new THREE.Scene();
 
     const geometry = new THREE.SphereGeometry( 200, 200, 200 );
-    const material = new THREE.MeshBasicMaterial( { color: "red" } );
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+
+    // load earth texture
+    const texLoader = new THREE.TextureLoader();
+    const texture = texLoader.load('textures/earth.jpg');
+    const material = new THREE.MeshBasicMaterial({map: texture});
+
+    earth = new THREE.Mesh( geometry, material );
+    scene.add( earth );
+
+
+    const light = new THREE.AmbientLight( 0xFFFFFF ); // soft white light
+    scene.add( light );
+
+
+
+    //load iss model
+    const objLoader = new GLTFLoader();
+    // Load a glTF resource
+    objLoader.load(
+        // resource URL
+        'models/ISS.glb',
+        // called when the resource is loaded
+        function ( gltf ) {
+            iss = gltf.scene;
+            scene.add( gltf.scene );
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene; // THREE.Group
+            gltf.scenes; // Array<THREE.Group>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+        },
+        // called while loading is progressing
+        function ( xhr ) {
+
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+        },
+        // called when loading has errors
+        function ( error ) {
+
+            console.log( 'An error happened' );
+
+        }
+    );
+
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor( 0x050505    , 1);
     document.body.appendChild( renderer.domElement );
 
     window.addEventListener( 'resize', onWindowResize );
@@ -41,8 +88,10 @@ function animate() {
 
     requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.005;
-    mesh.rotation.y += 0.01;
+    earth.rotation.x += 0.005;
+    earth.rotation.y += 0.01;
+
+    console.log(iss);
 
     renderer.render( scene, camera );
 
